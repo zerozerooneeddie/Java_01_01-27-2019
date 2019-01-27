b212a4fa6b932a9657f38cc565511e178c3c8634
zerozerooneeddie <bautistaeddielyn12@gmail.com>
2019-01-27 15:24:29 +0800

    to declare  and initialize variables

---
+++ VariableSamples.java
@@ -0,0 +1,42 @@
+
+public class VariableSamples {
+	
+		public static void main (String[] args ){
+		//declare a data type with variable name
+		// result and boolean data type
+		boolean result = true;
+
+		//declare a data type with variable name
+		//option and char data type
+
+		char option;
+
+		option = 'C';// assign 'C' to option
+
+		//declare a data type with variable name
+		//grade, double data type and initialized
+		//to 0.0
+
+		double grade = 0.0;
+
+		//Using counter increment/ decrement
+		int counter = 5;
+
+		//System.out.println("Result: " + result);
+		//System.out.println("Option: " + option);
+		//System.out.println("Grade: " + grade);
+		System.out.println("Counter: " + counter);
+
+		result = false;
+		option = 'G';
+		grade = 1.0;
+		counter++;
+
+
+		//System.out.println("Result: " + result);
+		//System.out.println("Option: " + option);
+		//System.out.println("Grade: " + grade);
+		System.out.println("Counter: " + counter);
+
+		}
+}
