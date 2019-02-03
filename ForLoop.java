31b4b38c3af54a10bfa4f0fda39aca46aee5f3f5
zerozerooneeddie <bautistaeddielyn12@gmail.com>
2019-02-03 15:00:05 +0800

    Using for loop

---
+++ ForLoop.java
@@ -0,0 +1,31 @@
+public class ForLoop{
+		public static void main(String[] args) 
+		{
+
+				//Nested loop//
+			//Displaying int triangle///
+
+				for (int i = 0; i< 5; i++)
+				{
+
+					for (int j = 0; j<=i; j++)
+					{
+
+						//if (j> i) break;//
+
+					System.out.print('*');
+
+				}
+
+					System.out.println(" ");
+				}
+
+
+					
+
+
+
+		}
+
+
+}
